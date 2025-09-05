03e96616bed6f8ce3d11e0a2ba8bc20d939749db
NoahLensink <134017421+NoahLensink@users.noreply.github.com>
2025-09-05 21:03:04 +0200

    Restore src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>5/3/1 Planner</h1>
+      <p>Welcome to your training planner!</p>
+    </div>
+  );
+}
+
+export default App;
