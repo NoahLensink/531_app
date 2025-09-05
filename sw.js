bad6ffb1357d11be26aa23e485f5d4b37257e585
NoahLensink <134017421+NoahLensink@users.noreply.github.com>
2025-09-05 21:02:53 +0200

    Restore sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('531-cache-v1').then((cache) =>
+      cache.addAll(['/', '/index.html', '/manifest.json'])
+    )
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => resp || fetch(e.request))
+  );
+});
